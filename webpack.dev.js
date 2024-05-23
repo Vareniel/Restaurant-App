fedd9aeaf4159a65ccaac2790be9347d9c27319b
Muhammad Fadhilah Hanan <88243095+Vareniel@users.noreply.github.com>
2024-05-23 23:54:23 +0700

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,47 @@
 /* eslint-disable no-undef */
-const { merge } = require("webpack-merge");
-const path = require("path");
-const common = require("./webpack.common");
+const path = require('path');
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    static: path.resolve(__dirname, "dist"),
+    static: path.resolve(__dirname, 'dist'),
     open: true,
     port: 9000,
-    compress: true,
     client: {
       overlay: {
         errors: true,
-        warnings: false,
+        warnings: true,
       },
     },
+    compress: true,
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ImageminWebpackPlugin({
+      plugins: [
+        ImageminMozjpeg({
+          quality: 50,
+          progressive: true,
+        }),
+      ],
+    }),
+    new ImageminWebpWebpackPlugin({
+      config: [
+        {
+          test: /\.(jpe?g|png)/,
+          options: {
+            quality: 40,
+          },
+        },
+      ],
+      overrideExtension: true,
+    }),
+  ],
 });
